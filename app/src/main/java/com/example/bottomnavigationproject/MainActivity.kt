f8cad809c40207b76f9412aa9c27a38c0d6eaaf8
loco-mkkhs <105632704+loco-mkkhs@users.noreply.github.com>
2022-10-04 09:25:44 +0300

    Bottom Navigation

---
+++ app/src/main/java/com/example/bottomnavigationproject/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.bottomnavigationproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        /* val bottomNavigationItemView = findViewById<BottomNavigationItemView>(R.id.bottomNavigationView)
+        val  navController = findNavController(R.id.fragment)
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.firstFragment, R.id.secondFragment, R.id.thirdFragment))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+
+        bottomNavigationItemView.setupWithNavontroller(navController)
+
+         */
+    }
+}
+
